4f945305aa7d9fe6790a05a5347a927f94a214f6
pengfei.zhou <pengfeizhou@foxmail.com>
2020-01-03 17:59:36 +0800

    update style css

--- web-playground/public/main-3.js
+++ web-playground/public/main-3.js
@@ -450,7 +450,9 @@ async function main() {
     automaticLayout: true,
     scrollBeyondLastLine: true,
     scrollBeyondLastColumn: 3,
-    formatOnType: true
+    formatOnType: true,
+    wordWrap: "on",
+    fontSize: 16
   };
 
   const State = {
@@ -729,7 +731,6 @@ async function main() {
       inputEditor.setModel(State.inputModel);
 
       UI.refreshOutput();
-      UI.renderSettings();
       UI.updateURL();
     },
 
@@ -757,8 +758,47 @@ async function main() {
       }
 
       return `
-const message: string = 'hello doric';
-console.log(message);
+import { Panel, Group, vlayout, layoutConfig, Gravity, IVLayout, text, Text, Color } from "doric";
+
+@Entry
+class HelloDoric extends Panel {
+    build(rootView: Group): void {
+        rootView.backgroundColor = Color.WHITE
+        let number: Text
+        let count = 0
+        vlayout([
+            number = text({
+                textSize: 40,
+                text: '0',
+            }),
+            text({
+                text: "Click to count",
+                textSize: 20,
+                backgroundColor: Color.parse('#70a1ff'),
+                textColor: Color.WHITE,
+                onClick: () => {
+                    number.text = \`${ ++count} \`
+                },
+                layoutConfig: layoutConfig().just(),
+                width: 200,
+                height: 50,
+            }),
+        ])
+            .apply({
+                layoutConfig: layoutConfig().just().configAlignment(Gravity.Center),
+                width: 200,
+                height: 200,
+                space: 20,
+                border: {
+                    color: Color.BLUE,
+                    width: 1,
+                },
+                gravity: Gravity.Center,
+            } as IVLayout)
+            .in(rootView)
+    }
+
+}
   `.trim();
     },
 
@@ -938,6 +978,7 @@ console.log(message);
 
             if (isDefaultInJS) outputText = "// In JS Mode for 3.7 and above, the playground will\n// show the d.ts for your JavaScript code"
             State.outputModel.setValue(outputText);
+            runJavaScript()
           });
         }
       });
@@ -952,11 +993,6 @@ console.log(message);
 
   UI.removeLoadingIndicator()
   UI.setCodeFromHash();
-
-  UI.renderSettings();
-  UI.fetchTooltips().then(() => {
-    UI.renderSettings();
-  });
 
   UI.openDropdownsOnLaunchIfNeeded()
 
@@ -1037,251 +1073,6 @@ console.log(message);
   function prettier() {
     inputEditor.getAction('editor.action.formatDocument').run()
   }
-
-  const exporter = () => {
-    function getScriptTargetText(option) {
-      if (option === monaco.languages.typescript.ScriptTarget.None) { return undefined; }
-      return monaco.languages.typescript.ScriptTarget[option];
-    }
-
-    function getJsxEmitText(option) {
-      if (option === monaco.languages.typescript.JsxEmit.None) { return undefined; }
-      return monaco.languages.typescript.JsxEmit[option];
-    }
-
-    function getModuleKindText(option) {
-      if (option === monaco.languages.typescript.ModuleKind.None) { return undefined; }
-      return monaco.languages.typescript.ModuleKind[option];
-    }
-
-    function getValidCompilerOptions(options) {
-      const { target: targetOption, jsx: jsxOption, module: moduleOption, ...restOptions } = options;
-
-      const targetText = getScriptTargetText(targetOption);
-      const jsxText = getJsxEmitText(jsxOption);
-      const moduleText = getModuleKindText(moduleOption);
-
-      return {
-        ...restOptions,
-        ...(targetText && { target: targetText }),
-        ...(jsxText && { jsx: jsxText }),
-        ...(moduleText && { module: moduleText }),
-      };
-    }
-
-    // Based on https://github.com/stackblitz/core/blob/master/sdk/src/generate.ts
-    function createHiddenInput(name, value) {
-      const input = document.createElement('input');
-      input.type = 'hidden';
-      input.name = name;
-      input.value = value;
-      return input;
-    }
-
-    function createProjectForm(project) {
-      const form = document.createElement('form');
-
-      form.method = 'POST';
-      form.setAttribute('style', 'display:none;');
-
-      form.appendChild(createHiddenInput('project[title]', project.title));
-      form.appendChild(createHiddenInput('project[description]', project.description));
-      form.appendChild(createHiddenInput('project[template]', project.template));
-
-      if (project.tags) {
-        project.tags.forEach(tag => {
-          form.appendChild(createHiddenInput('project[tags][]', tag));
-        });
-      }
-
-      if (project.dependencies) {
-        form.appendChild(createHiddenInput('project[dependencies]', JSON.stringify(project.dependencies)));
-      }
-
-      if (project.settings) {
-        form.appendChild(createHiddenInput('project[settings]', JSON.stringify(project.settings)));
-      }
-
-      Object.keys(project.files).forEach(path => {
-        form.appendChild(createHiddenInput(`project[files][${path}]`, project.files[path]));
-      });
-
-      return form;
-    }
-
-    const typescriptVersion = (window.ts && window.ts.version) || window.CONFIG.TSVersion
-    const stringifiedCompilerOptions = JSON.stringify({ compilerOptions: getValidCompilerOptions(compilerOptions) }, null, "  ");
-
-    // TODO: pull deps
-    function openProjectInStackBlitz() {
-      const project = {
-        title: "Playground Export - ",
-        description: "123",
-        template: "typescript",
-        files: {
-          "index.ts": State.inputModel.getValue(),
-          "tsconfig.json": stringifiedCompilerOptions,
-        },
-        dependencies: {
-          "typescript": typescriptVersion
-        }
-
-      }
-      const form = createProjectForm(project);
-      form.action = "https://stackblitz.com/run?view=editor"
-      // https://github.com/stackblitz/core/blob/master/sdk/src/helpers.ts#L9
-      // + buildProjectQuery(options);
-      form.target = '_blank'
-
-      document.body.appendChild(form);
-      form.submit();
-      document.body.removeChild(form);
-    }
-
-    function openInTSAST() {
-      const hash = `#code/${LZString.compressToEncodedURIComponent(State.inputModel.getValue())}`;
-      document.location = `https://ts-ast-viewer.com/${hash}`
-    }
-
-    function openProjectInCodeSandbox() {
-      const files = {
-        "package.json": {
-          content: {
-            name: "TypeScript Playground Export",
-            version: "0.0.0",
-            description: "TypeScript playground exported Sandbox",
-            dependencies: {
-              "typescript": typescriptVersion
-            }
-          }
-        },
-        "index.ts": {
-          content: State.inputModel.getValue()
-        },
-        "tsconfig.json": {
-          content: stringifiedCompilerOptions
-        }
-      };
-
-      // Using the v1 get API
-      const parameters = LZString.compressToBase64(JSON.stringify({ files }))
-        .replace(/\+/g, "-") // Convert '+' to '-'
-        .replace(/\//g, "_") // Convert '/' to '_'
-        .replace(/=+$/, ""); // Remove ending '='
-
-      const url = `https://codesandbox.io/api/v1/sandboxes/define?view=editor&parameters=${parameters}`;
-      document.location = url
-
-      // Alternative using the http URL API, which uses POST. This has the trade-off where
-      // the async nature of the call means that the redirect at the end triggers
-      // popup security mechanisms in browsers because the function isn't blessed as
-      // being a direct result of a user action.
-
-      // fetch("https://codesandbox.io/api/v1/sandboxes/define?json=1", {
-      //   method: "POST",
-      //   body: JSON.stringify({ files }),
-      //   headers: {
-      //     Accept: "application/json",
-      //     "Content-Type": "application/json"
-      //   }
-      // })
-      // .then(x => x.json())
-      // .then(data => {
-      //   window.open('https://codesandbox.io/s/' + data.sandbox_id, '_blank');
-      // });
-
-    }
-
-    function codify(code, ext) {
-      return "```" + ext + "\n" + code + "\n```\n"
-    }
-
-    function makeMarkdown() {
-      return `
-<!-- 🚨 STOP 🚨 𝗦𝗧𝗢𝗣 🚨 𝑺𝑻𝑶𝑷 🚨
-
-Half of all issues filed here are duplicates, answered in the FAQ, or not appropriate for the bug tracker. Even if you think you've found a *bug*, please read the FAQ first, especially the Common "Bugs" That Aren't Bugs section!
-
-Please help us by doing the following steps before logging an issue:
-  * Search: https://github.com/Microsoft/TypeScript/search?type=Issues
-  * Read the FAQ: https://github.com/Microsoft/TypeScript/wiki/FAQ
-
-Please fill in the *entire* template below.
--->
-
-**TypeScript Version:**  ${typescriptVersion}
-
-<!-- Search terms you tried before logging this (so others can find this issue more easily) -->
-**Search Terms:**
-
-**Expected behavior:**
-
-**Actual behavior:**
-
-<!-- Did you find other bugs that looked similar? -->
-**Related Issues:**
-
-**Code**
-${codify(State.inputModel.getValue(), "ts")}
-
-
-<details><summary><b>Output</b></summary>
-
-${codify(State.outputModel.getValue(), "ts")}
-
-</details>
-
-
-<details><summary><b>Compiler Options</b></summary>
-
-${codify(stringifiedCompilerOptions, "json")}
-
-</details>
-
-**Playground Link:** [Provided](${window.location})
-      `
-    }
-
-    function reportIssue() {
-      const body = makeMarkdown();
-      if (body.length < 4000) {
-        window.open('https://github.com/Microsoft/TypeScript/issues/new?body=' + encodeURIComponent(body))
-      } else {
-        UI.showModal(body, "Issue too long to post automatically, you can copy here then click below", { "Create New Issue": "https://github.com/Microsoft/TypeScript/issues/new" })
-      }
-    }
-
-    function copyAsMarkdownIssue() {
-      const markdown = makeMarkdown();
-      UI.showModal(markdown)
-    }
-
-    function copyForChat() {
-      const chat = `[Playground Link](${window.location})`
-      UI.showModal(chat)
-    }
-
-    function copyForChatWithPreview() {
-      const ts = State.inputModel.getValue()
-      const preview = (ts.length > 200) ? ts.substring(0, 200) + "..." : ts.substring(0, 200)
-
-      const code = "```\n" + preview + "\n```\n"
-      const chat = `${code}\n[Playground Link](${window.location})`
-      UI.showModal(chat)
-    }
-
-    return {
-      openProjectInStackBlitz,
-      openProjectInCodeSandbox,
-      reportIssue,
-      copyAsMarkdownIssue,
-      copyForChat,
-      copyForChatWithPreview,
-      openInTSAST
-    }
-  }
-
-  window.exporter = exporter()
 }
 
 
